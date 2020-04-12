c35c294893360030c44f61b0153f1a0b444a110a
Ivan Angelov <angelow.van@yandex.ru>
2020-04-13 02:43:49 +0300

    Added class CLIHandler and routing to it's functions for command line options

---
+++ src/CLIHandler.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace project {
+
+    public static class CLIHandler {
+        public static void showHelp() {
+            
+        }
+
+        public static void handleStringConversion(string[] args) {
+
+        }
+
+        public static void handleFileConversion(string[] args) {
+
+        }
+    }
+
+}
